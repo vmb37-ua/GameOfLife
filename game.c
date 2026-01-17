1ee8ebbf43b4cd2b5b66a3fe1abca2ef7232ecf3
vmb37-ua <vmb37@alu.ua.es>
2026-01-17 20:25:11 +0100

    Arreglado error dibujar

--- game.c
+++ game.c
@@ -3,22 +3,51 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define WIDTH 900
-#define HEIGHT 600
+#define WIDTH 1920
+#define HEIGHT 1080
+
+void dibujarPixeles(char* estado, SDL_Surface* superf){
+    SDL_Rect* pixel = &(SDL_Rect){100,100,1,1};
+    for(int i = 0; i < WIDTH*HEIGHT/8; i++){
+        char byte = estado[i];
+        for(int j = 0; j < 8; j++){
+            char res = (char)0x80 & (byte << j);
+            if (res == (char)0x80){
+                pixel->x= ((i*8+j) % WIDTH);
+                pixel->y= ((i*8+j) / WIDTH);
+                SDL_FillRect(superf, pixel, 0xFFFFFFFF);
+                //printf("Dibujado pixel en %d, %d: que viene de num %d\n", pixel->x, pixel->y, i*8+j);
+            }
+        }
+    }
+}
+
+void pasoDeTiempo(char* estado1, char* estado2){
+    
+}
 
 int main(int argc, char* argv[]){
     SDL_Window* ventana = SDL_CreateWindow("Juego de la vida de Conway",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT, 0);  
     SDL_Surface *superf = SDL_GetWindowSurface(ventana);
 
+    char* estado1 = (char *)malloc((WIDTH*HEIGHT)/8);
+    memset(estado1, 0, WIDTH*HEIGHT/8);
+
+    char* estado2 = (char *)malloc((WIDTH*HEIGHT)/8);
+    memset(estado2, 0, WIDTH*HEIGHT/8);
+
+    char letra = 0x00;
 
     bool app_running = true;
-    
+
     while(app_running){
         SDL_Event event;
 
         SDL_FillRect(superf,&(SDL_Rect){0,0,WIDTH,HEIGHT}, 0x00000000);
 
-        // Logica de actualizacion de pixeles
+        dibujarPixeles(estado1, superf);
+        memset(estado1, letra++, WIDTH*HEIGHT/8);
+        //estado1[1] = letra++;
 
         SDL_UpdateWindowSurface(ventana);
 
@@ -27,6 +56,8 @@ int main(int argc, char* argv[]){
                 app_running = 0;
             }
         }
-        SDL_Delay(50);
+        SDL_Delay(100);
     }
+
+    free(estado1);
 }
