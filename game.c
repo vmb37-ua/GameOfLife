2b9d6c4f46ea3e39f3489f67f738e8a060cf9bed
vmb37-ua <vmb37@alu.ua.es>
2026-01-17 16:52:07 +0100

    SDLbase hecho

--- game.c
+++ game.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#define WIDTH 900
+#define HEIGHT 600
 
 int main(int argc, char* argv[]){
-    printf("Hola mundo");
+    SDL_Window* ventana = SDL_CreateWindow("Juego de la vida de Conway",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT, 0);  
+    SDL_Surface *superf = SDL_GetWindowSurface(ventana);
+
+
+    bool app_running = true;
+    
+    while(app_running){
+        SDL_Event event;
+
+        SDL_FillRect(superf,&(SDL_Rect){0,0,WIDTH,HEIGHT}, 0x00000000);
+
+        // Logica de actualizacion de pixeles
+
+        SDL_UpdateWindowSurface(ventana);
+
+        while(SDL_PollEvent(&event)){
+            if(event.type == SDL_QUIT){
+                app_running = 0;
+            }
+        }
+        SDL_Delay(50);
+    }
 }
